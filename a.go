a705871c97ad4068b115f76c033586e85734e797
Zanggar Zhuamagaliyev <200103287@stu.sdu.edu.kz>
2023-02-06 10:57:23 +0600

    2

--- a.go
+++ a.go
@@ -1,3 +1,38 @@
 package a
 
-const X = "Learn"
+import (
+	"fmt"
+	"strconv"
+)
+
+var Products []Product
+var Users []User
+
+type User struct {
+	ID       int
+	FullName string
+}
+
+type Product struct {
+	ID     int
+	Title  string
+	UserID int
+}
+
+func AddUser(name string) {
+	user := User{len(Users), name}
+	Users = append(Users, user)
+}
+
+func AddProduct(title string, userID int) {
+	product := Product{len(Products), title, userID}
+	Products = append(Products, product)
+}
+
+func (user User) Print() {
+	fmt.Println("ID: " + strconv.Itoa(user.ID) + ", Full Name: " + user.FullName)
+}
+
+func (product Product) Print() {
+	fmt.Println("ID: " + strconv.Itoa(product.ID) + ", Title: " + product.Title)
+}
